105111cc5b496d3a9914787887d9c8f3dbc671aa
Xavier Tseng <364671459@qq.com>
2014-11-21 11:03:28 +0800

    Update d2fw.lua

--- d2fw.lua
+++ d2fw.lua
@@ -5,3 +5,11 @@ require('D2FW/XavierCHN/GameRules')
 --[Includes 包含]
 -- PrecacheEveryThingFromKV( context ) https://github.com/XavierCHN/Dota2FrameWork/blob/master/D2FW/XavierCHN/ResourcePrecacher.lua#L1
 require('D2FW/XavierCHN/ResourcePrecacher')
+
+--[Includes 包含]
+-- [[
+    (1) BuildingHelper:AddBuildingToGrid(vPoint, nSize, vOwnersHero)
+    (2) BuildingHelper:AddBuilding(building)
+    (3) BuildingHelper:AddUnit(unit)
+]]
+require('D2FW/MyII/BuildingHelper')
