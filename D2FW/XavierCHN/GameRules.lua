bce46579f6fe32cc9e932ef98b8d9c5bd3d08c82
Xavier Tseng <364671459@qq.com>
2014-11-21 10:21:01 +0800

    Update and rename GeneralGameRule.lua to GameRules.lua

--- D2FW/XavierCHN/GameRules.lua
+++ D2FW/XavierCHN/GameRules.lua
@@ -1,3 +1,7 @@
+--[Comment]
+-- Set the game mode as anyteam achieve nKillsToWin kills to become the winner of the game.
+-- This function was suggested to called in addon_game_mode.lua@InitGameMode()
+-- nKillsToWin: int the number of kills to win the game mode.
 -- 设置游戏为某队死亡nKillsToWin次之后，对方就获得胜利的模式
 -- 建议放在InitGameMode中
 -- nKillsToWin: int 队伍获胜所需要击杀的人头数
@@ -5,13 +9,13 @@
 function GameRules:SetNumKillsToWinGameRule(nKillsToWin)
     self.__nKillsToWin = nKillsToWin
     -- 监听玩家被击杀的事件
-    ListenToGameEvent("dota_player_killed", Dynamic_Wrap( amhc.XavierCHN.GeneralGameRule, "OnHeroKilled"), self)
+    ListenToGameEvent("dota_player_killed", Dynamic_Wrap( amhc.XavierCHN.GeneralGameRule, "__XavierCHN__OnHeroKilled"), self)
 end
 
 -- 当玩家被击杀[N次杀敌获胜游戏模式的击杀响应]
 -- keys: table 游戏自动生成的事件参数表
 -- 
-function GameRules:OnHeroKilled(keys)
+function GameRules:__XavierCHN__OnHeroKilled(keys)
     -- 获取死亡玩家ID
     local player_id = keys.PlayerID
     -- 获取死亡玩家实体
