a375e9434cba6dc6c05307c3c391fc5f0915bfd3
Xavier Tseng <364671459@qq.com>
2014-11-21 10:25:30 +0800

    Update d2fw.lua

--- d2fw.lua
+++ d2fw.lua
@@ -1,1 +1,7 @@
-require('XavierCHN/GeneralGameRules')
+--[Includes 包含]
+-- GameRules:SetNumKillsToWinGameRule(nKillsToWin) https://github.com/XavierCHN/Dota2FrameWork/blob/master/D2FW/XavierCHN/GameRules.lua#L1
+require('D2FW/XavierCHN/GameRules')
+
+--[Includes 包含]
+-- PrecacheEveryThingFromKV( context ) https://github.com/XavierCHN/Dota2FrameWork/blob/master/D2FW/XavierCHN/ResourcePrecacher.lua#L1
+require('D2FW/XavierCHN/ResourcePrecacher')
