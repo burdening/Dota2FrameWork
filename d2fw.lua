4795142733f44077fd26e02bba35a0148334124d
Xavier Tseng <364671459@qq.com>
2014-12-01 00:07:17 +0800

    Update d2fw.lua

--- d2fw.lua
+++ d2fw.lua
@@ -13,3 +13,10 @@ require('D2FW/XavierCHN/ResourcePrecacher')
     (3) BuildingHelper:AddUnit(unit)
 ]]
 require('D2FW/MyII/BuildingHelper')
+
+require('D2FW/XavierCHN/ParaAdjuster')
+ParaAdjuster:Init()
+--	ParaAdjuster:SetStrToHealth(30) -- set each point of strength give 30 point of health
+--	ParaAdjuster:SetIntToMana(40) -- set each point of intellegence give 40 point of mana
+--	ParaAdjuster:SetAgiToAtkSpd(50) -- set each point of agility give 50 point of attack speed bonus
+--	ParaAdjuster:SetAgiToAmr(60) -- set each point of agility give 60 point of mana
