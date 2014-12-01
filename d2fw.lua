28f2b16983484d37640b8b09814a17599f08fc9f
Xavier Tseng <364671459@qq.com>
2014-12-01 12:15:35 +0800

    Update d2fw.lua

--- d2fw.lua
+++ d2fw.lua
@@ -12,7 +12,7 @@ require('D2FW/XavierCHN/ResourcePrecacher')
     (2) BuildingHelper:AddBuilding(building)
     (3) BuildingHelper:AddUnit(unit)
 ]]
-require('D2FW/MyII/BuildingHelper')
+require('D2FW/Myll/BuildingHelper')
 
 require('D2FW/XavierCHN/ParaAdjuster')
 ParaAdjuster:Init()
